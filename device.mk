AOSPA/android_device_samsung_manta

commit e87ec97b70350c318b488f3bc2a26b5a70d53e18

wlan: Add wlan initial configuration
Change-Id: I9b127fe5bf9665984186ce439d9eeca519ebd0c3
Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

device.mk
@@ -47,7 +47,11 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
 	frameworks/base/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
-	frameworks/base/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
+	frameworks/base/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+	frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml
+
+PRODUCT_COPY_FILES += \
+	device/samsung/manta/bcmdhd.cal:system/etc/wifi/bcmdhd.cal
 
 PRODUCT_PACKAGES := \
         make_ext4fs \
@@ -82,3 +86,8 @@ PRODUCT_PACKAGES += \
 $(call inherit-product-if-exists, vendor/samsung_slsi/exynos5/exynos5-vendor.mk)
 $(call inherit-product-if-exists, vendor/samsung/manta/device-vendor.mk)
 $(call inherit-product, frameworks/base/build/tablet-dalvik-heap.mk)
+
+PRODUCT_PROPERTY_OVERRIDES := \
+	wifi.interface=wlan0
+
+$(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4324/device-bcm.mk)

BoardConfig.mk
@@ -52,3 +52,14 @@ BOARD_FLASH_BLOCK_SIZE := 4096
 #TARGET_PROVIDES_INIT_RC := true
 #TARGET_USERIMAGES_SPARSE_EXT_DISABLED := true
 
+# Wifi related defines
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+WPA_SUPPLICANT_VERSION      := VER_0_8_X
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_DRIVER        := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB   := lib_driver_cmd_bcmdhd
+BOARD_WLAN_DEVICE           := bcmdhd
+WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA     := "/vendor/firmware/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_P2P     := "/vendor/firmware/fw_bcmdhd_p2p.bin"
+WIFI_DRIVER_FW_PATH_AP      := "/vendor/firmware/fw_bcmdhd_apsta.bin"

init.manta.rc
@@ -10,6 +10,10 @@ on post-fs-data
     mkdir /data/media 0775 media_rw media_rw
     chown media_rw media_rw /data/media
     setprop vold.post_fs_data_done 1
+    mkdir /data/misc/wifi 0770 wifi wifi
+    mkdir /data/misc/wifi/sockets 0770 wifi wifi
+    mkdir /data/misc/dhcp 0770 dhcp dhcp
+    chown dhcp dhcp /data/misc/dhcp
 
 on boot
     mount debugfs /sys/kernel/debug /sys/kernel/debug
@@ -34,3 +38,24 @@ on fs
 # underlying files in /data/media will be created with user and group media_rw (1023)
 service sdcard /system/bin/sdcard /data/media 1023 1023
     class late_start
+
+service wpa_supplicant /system/bin/wpa_supplicant \
+   -iwlan0 -Dnl80211 -c/data/misc/wifi/wpa_supplicant.conf -e/data/misc/wifi/entropy.bin
+    #   we will start as root and wpa_supplicant will switch to user wifi
+    #   after setting up the capabilities required for WEXT
+    #   user wifi
+    #   group wifi inet keystore
+    class main
+    socket wpa_wlan0 dgram 660 wifi wifi
+    disabled
+    oneshot
+
+service dhcpcd_wlan0 /system/bin/dhcpcd -aABKL
+    class main
+    disabled
+    oneshot
+
+service iprenew_wlan0 /system/bin/dhcpcd -n
+    class main
+    disabled
+    oneshot

commit 2bfcc2828a54d52a26563a76314fe98a10700eae

manta: specify property overrides *before* inheriting from products
fixes bug introduced in e87ec97b70350c318b488f3bc2a26b5a70d53e18

When doing PRODUCT_xxxx :=  lines after inheriting from products,
the whole value is replaced, that's not what you want.

Never put any PRODUCT_xxx variable settings after inheriting products
unless you know what you are doing.

Change-Id: Iba95d5f066ede4e9e30c6a7aaf7bf47c86e7366b
Signed-off-by: Dima Zavin <dima@android.com>

device.mk
@@ -86,12 +86,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     make_ext4fs
 
-$(call inherit-product-if-exists, vendor/samsung_slsi/exynos5/exynos5-vendor.mk)
-$(call inherit-product-if-exists, vendor/samsung/manta/device-vendor.mk)
-$(call inherit-product, frameworks/native/build/tablet-dalvik-heap.mk)
-
 PRODUCT_PROPERTY_OVERRIDES := \
     wifi.interface=wlan0 \
     ro.sf.lcd_density=320
+
+$(call inherit-product-if-exists, vendor/samsung_slsi/exynos5/exynos5-vendor.mk)
+$(call inherit-product-if-exists, vendor/samsung/manta/device-vendor.mk)
+$(call inherit-product, frameworks/native/build/tablet-dalvik-heap.mk)
 
 $(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4324/device-bcm.mk)